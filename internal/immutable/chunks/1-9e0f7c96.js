910fc8b334d3cf492f56f8d51f268538534d5df9
Egor Konovalov <konovalov.ye@gmail.com>
2022-10-12 17:55:26 +0400

    Updates

---
+++ internal/immutable/chunks/1-9e0f7c96.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-dca850c8.js";export{t as component};
--- internal/immutable/chunks/2-8210fe2e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-95a40258.js";export{t as component};
