24f820ef925439a2d5681202b97b051631dc1e63
Egor Konovalov <konovalov.ye@gmail.com>
2022-10-16 18:18:15 +0400

    ref: refactor main page
    so it will show nothing before the environment is loaded

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,21 @@
   import { TelegramEnvironment } from "$lib/modules/platformenvironment/classes/TelegramEnvironment.class";
   import { PlatformEnvironmentService } from "$lib/modules/platformenvironment/platformenvironment.service";
   import Main from "../components/Main.svelte";
-  let environment = PlatformEnvironmentService.getEnvironment();
+
+  async function getEnvironment() {
+    return PlatformEnvironmentService.getEnvironment();
+  }
 </script>
 
-{#if environment instanceof TelegramEnvironment}
-  <Main />
-{:else}
-  <p class="w-full text-center p-10 font-semibold leading-loose">
-    You can use this website only with a<br /><a
-      class="text-blue-700 underline"
-      href="https://t.me/album_saver_web_tes_bot">Telegram bot</a
-    >
-  </p>
-{/if}
+{#await getEnvironment() then environment}
+  {#if environment instanceof TelegramEnvironment}
+    <Main />
+  {:else}
+    <p class="w-full text-center p-10 font-semibold leading-loose">
+      You can use this website only with a<br /><a
+        class="text-blue-700 underline"
+        href="https://t.me/album_saver_web_tes_bot">Telegram bot</a
+      >
+    </p>
+  {/if}
+{/await}
