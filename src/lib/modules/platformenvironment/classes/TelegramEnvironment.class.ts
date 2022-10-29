e7a3900156d4d528baf2f09da95b175942e7396f
Egor Konovalov <konovalov.ye@gmail.com>
2022-10-29 14:24:59 +0400

    fix: fix dark mode

--- src/lib/modules/platformenvironment/classes/TelegramEnvironment.class.ts
+++ src/lib/modules/platformenvironment/classes/TelegramEnvironment.class.ts
@@ -3,13 +3,17 @@ import { QueryStringParser } from "$lib/utils/QueryStringParser.utility";
 
 export class
   TelegramEnvironment implements PlatformEnvironment {
+  readonly _webApp = Telegram.WebApp
   readonly _userId: number;
   readonly _platformName: string;
+  readonly _colorScheme: string;
   constructor() {
     const data = QueryStringParser.parseString(Telegram.WebApp.initData);
     const user = QueryStringParser.parseJson(data.user);
     this._userId = user.id;
-    this._platformName = data.platform
+    this._platformName = data.platform;
+    this._colorScheme = Telegram.WebApp.colorScheme;
+    this.applyColorScheme()
   };
 
   get userId(): number {
@@ -19,4 +23,16 @@ export class
   get platformName(): string {
     return this._platformName;
   };
+
+  get colorScheme(): string {
+    return this._colorScheme;
+  }
+
+  applyColorScheme() {
+    if (this._webApp.colorScheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
   theme: {
     extend: {}
   },
