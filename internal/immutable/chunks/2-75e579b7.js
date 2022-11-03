f60bda3871380ebf80050f0a7add354168124e90
Egor Konovalov <konovalov.ye@gmail.com>
2022-11-03 14:39:26 +0400

    Updates

--- internal/immutable/chunks/2-75e579b7.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-a4eb5d38.js";export{t as component};
---
+++ internal/immutable/chunks/2-9f96552f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-fc503afe.js";export{t as component};
