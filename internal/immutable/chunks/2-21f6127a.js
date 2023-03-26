76ce908068f35c65559230544424bddbbaec6172
Egor Konovalov <konovalov.ye@gmail.com>
2023-03-26 08:49:13 +0400

    Updates

---
+++ internal/immutable/chunks/2-21f6127a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-5d4d57ce.js";export{t as component};
--- internal/immutable/chunks/2-9c151f2f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-3974bd63.js";export{t as component};
