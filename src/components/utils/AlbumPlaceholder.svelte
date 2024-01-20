f79a1bed157197a92302eb9de6785cc4043ea35c
Egor <konovalov.ye@gmail.com>
2024-01-20 19:52:59 +0100

    chore: update tg api; fix records grid alignment

--- src/components/utils/AlbumPlaceholder.svelte
+++ src/components/utils/AlbumPlaceholder.svelte
@@ -1,3 +1,9 @@
-<span class="artwork w-52 h-60 bg-stone-200 animate-pulse" />
-<span class="h-5 w-20 rounded-md self-center bg-stone-200 animate-pulse my-3" />
-<span class="h-3 w-32 rounded-md self-center bg-stone-200 animate-pulse" />
+<span
+  class="artwork bg-[color:var(--tg-theme-secondary-bg-color)] animate-pulse aspect-square"
+/>
+<span
+  class="h-5 w-20 rounded-md self-center bg-[color:var(--tg-theme-secondary-bg-color)] animate-pulse my-3"
+/>
+<span
+  class="h-3 w-32 rounded-md self-center bg-[color:var(--tg-theme-secondary-bg-color)] animate-pulse"
+/>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [
 		sveltekit(),
-		basicSsl()
-	],
-	server: {
-		port: 80,
-		host: true
-	}
+	]
 };
 
 export default config;
