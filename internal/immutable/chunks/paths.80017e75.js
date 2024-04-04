13c7c5af36482b3352af276e19ffa6ab92497e09
egorkonovalov <egorkonovalov@users.noreply.github.com>
2024-04-04 11:21:40 +0000

    deploy: 004f047d45635a4b755808f1782d7eae9f8eec75

--- internal/immutable/chunks/paths.80017e75.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1bd1nv7)==null?void 0:s.base)??"/album-saver-web-v2";var a;const b=((a=globalThis.__sveltekit_1bd1nv7)==null?void 0:a.assets)??e;export{b as a,e as b};
