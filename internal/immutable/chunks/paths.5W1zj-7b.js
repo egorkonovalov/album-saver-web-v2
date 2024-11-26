08a9976dac549645523063a710bc6f8501827cc5
egorkonovalov <egorkonovalov@users.noreply.github.com>
2024-11-26 15:02:57 +0000

    deploy: baf1823b62b2f559d07db23ad3be4c3556c7f46c

--- internal/immutable/chunks/paths.5W1zj-7b.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1j3ul0e)==null?void 0:s.base)??"/album-saver-web-v2";var e;const l=((e=globalThis.__sveltekit_1j3ul0e)==null?void 0:e.assets)??a;export{l as a,a as b};
---
+++ internal/immutable/entry/start.LEuMoIOk.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.gSBun_94.js";export{t as start};
